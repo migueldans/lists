ff847e8e306ff7e04129b215dec15a29fb7d5040
migueldans <39558093+migueldans@users.noreply.github.com>
2018-10-18 10:56:52 +0200

    commit

---
+++ 1/1/CantanteFamoso.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class CantanteFamoso
+    {
+        private String nombre, discoConMasVentas;
+
+        //public CantanteFamoso()
+        //{
+
+        //}
+
+        public CantanteFamoso(string nombre, string discoConMasVentas)
+        {
+            this.nombre = nombre;
+            this.discoConMasVentas = discoConMasVentas;
+        }
+
+        //get-set
+
+        public string GetNombre()
+        {
+            return nombre;
+        }
+        public void SetNombre(string nombre)
+        {
+            this.nombre = nombre;
+        }
+        public string GetdiscoConMasventas()
+        {
+            return discoConMasVentas;
+        }
+        public void SetdiscoConMasVentas(string discoConMasVentas)
+        {
+            this.discoConMasVentas = discoConMasVentas;
+        }
+
+        public static CantanteFamoso CreaCantante()
+        {
+            Console.WriteLine("Introduzca el nombre de un/a cantante famoso/a.");
+            string introNombre = Console.ReadLine();
+
+            Console.WriteLine("Introduzca el nombre de un/a cantante famoso/a.");
+            string introDisco = Console.ReadLine();
+
+            return new CantanteFamoso(introNombre, introDisco);
+        }
+
+
+    }
+    
+
+
+}
+
--- 1/1/Program.cs
+++ 1/1/Program.cs
@@ -9,6 +9,8 @@ namespace _1
     class Program
     {
         static void Main(string[] args)
+
+            ////+++++++++++++++++++++++++++++++++++++ejercicio 1++++++++++++++++++++++++++++
         //{
         //    List<string> compra = new List<string>();
         //    CrearLista(compra);
@@ -54,65 +56,122 @@ namespace _1
         //{
         //    Console.Write(compra.Count());
         //}
+        ////++++++++++++++++++++++++++++++++++++++++ejercicio 2++++++++++++++++++++++++++++++++
+        //{
+
+        //    List<double> decimales = new List<double>();
+        //    List<double> enteros = new List<double>();
+
+
+        //    bool negativo = false;
+        //    try
+        //    {
+        //        while (negativo == false)
+        //        {
+        //            Console.WriteLine("Introduzca un numero");
+
+        //            double numero = Convert.ToDouble(Console.ReadLine());
+        //            string checkNumero = numero.ToString();
+        //            {
+        //                if (numero < 0)
+        //                {
+        //                    negativo = true;
+        //                }
+        //                else if (checkNumero.Contains(","))
+        //                {
+        //                    decimales.Add(numero);
+        //                }
+        //                else
+        //                {
+        //                    int nochecknumero = Convert.ToInt32(numero);
+        //                    enteros.Add(nochecknumero);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch (FormatException ex)
+        //    {
+        //        Console.WriteLine("El caracter introducido no es un numero.");
+        //    }
+
+        //    foreach (double x in decimales)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+
+        //    foreach (int x in enteros)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //    Console.ReadLine();
+
+        //    Console.WriteLine(decimales.Sum()+enteros.Sum());
+        //    double suma = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.WriteLine(decimales.Count() + enteros.Count());
+        //    int cuenta = Convert.ToInt32(Console.ReadLine());
+
+        //    Console.WriteLine(suma / cuenta);
+        //    Console.ReadLine();
+
+
+
+        //}
+        ////+++++++++++++++++++++++++++++++Ejericio 3+++++++++++++++++++++++++++++++++
         {
-            
-            List<double> decimales = new List<double>();
-            List<double> enteros = new List<double>();
+            CantanteFamoso cantante1 = new CantanteFamoso("David Hasselhof", "Looking for Freedom");
+            CantanteFamoso cantante2 = new CantanteFamoso("Emilio Aragón", "Te Huelen los Pies");
 
-            
-            bool negativo = false;
-            try
+            //CantanteFamoso cantantes = new CantanteFamoso();
+
+
+            List<CantanteFamoso> listaCantantesFamosos = new List<CantanteFamoso>
             {
-                while (negativo == false)
+                cantante1,
+                cantante2
+            };
+
+            Show(listaCantantesFamosos);
+
+            bool salir = false;
+            while(salir == false)
                 {
-                    Console.WriteLine("Introduzca un numero");
-
-                    double numero = Convert.ToDouble(Console.ReadLine());
-                    string checkNumero = numero.ToString();
-                    {
-                        if (numero < 0)
-                        {
-                            negativo = true;
-                        }
-                        else if (checkNumero.Contains(","))
-                        {
-                            decimales.Add(numero);
-                        }
-                        else
-                        {
-                            int nochecknumero = Convert.ToInt32(numero);
-                            enteros.Add(nochecknumero);
-                        }
-                    }
+                Console.WriteLine("¿Desea seguir?");
+                string respuesta = Console.ReadLine();
+                     if (respuesta.ToUpper() =="SI")
+                     {
+                     listaCantantesFamosos.Add(CantanteFamoso.CreaCantante());
+                     }
+                     else if(respuesta.ToUpper() == "NO")
+                     {
+                     salir = true;
+                     
+                     }
                 }
-            }
-            catch (FormatException ex)
-            {
-                Console.WriteLine("El caracter introducido no es un numero.");
-            }
 
-            foreach (double x in decimales)
-            {
-                Console.WriteLine(x);
-            }
-
-            foreach (int x in enteros)
-            {
-                Console.WriteLine(x);
-            }
-            Console.ReadLine();
-
-            Console.WriteLine(decimales.Sum()+enteros.Sum());
-            double suma = Convert.ToDouble(Console.ReadLine());
-
-            Console.WriteLine(decimales.Count() + enteros.Count());
-            int cuenta = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine(suma / cuenta);
-            Console.ReadLine();
-
-
+            Show(listaCantantesFamosos);
 
         }
+        public static void Show(List<CantanteFamoso>listaCantantesFamosos)
+        {
+            foreach(CantanteFamoso cantante in listaCantantesFamosos)
+            {
+                Console.WriteLine(cantante.GetNombre() + ", " + cantante.GetdiscoConMasventas());
+            }
+            Console.ReadLine();
+        }
+
+        //public static CantanteFamoso CreaCantante()
+        //{
+        //    Console.WriteLine("Introduzca el nombre de un/a cantante famoso/a.");
+        //    string introNombre = Console.ReadLine();
+
+        //    Console.WriteLine("Introduzca el nombre de un/a cantante famoso/a.");
+        //    string introDisco = Console.ReadLine();
+
+        //    CantanteFamoso cIntro = new CantanteFamoso(introNombre, introDisco);
+
+        //    return cIntro;
+        //}
     }
 }
